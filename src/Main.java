a046b4d223a719b40245c9a33300fcfce5f27db5
KarlisKocins <karlis.kocins@edu.rtu.lv>
2023-03-30 01:06:38 +0300

    all functions finished

--- src/Main.java
+++ src/Main.java
@@ -2,55 +2,67 @@ import java.io.*;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.Iterator;
-import java.util.List;
-import java.util.Scanner;
+import java.util.*;
 
 public class Main {
     static String FILE_NAME = "db.csv";
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        String command;
-        do {
-            System.out.print("Enter command: ");
-            command = input.nextLine().trim().toLowerCase();
-            String[] command_full = command.toString().split(" ",2);
+        String command = null;
+            do {
+                try {
+                System.out.print("Enter command: ");
+                command = input.nextLine().trim().toLowerCase();
+                String[] command_full = command.toString().split(" ", 2);
 
-            switch (command_full[0]) {
-                case "print":
-                    print();
-                    break;
-                case "add":
-                    add(command_full[1]);
-                    break;
-                case "del":
-                    del(command_full[1]);
-                    break;
-                case "edit":
-                    edit(command_full[1]);
-                    break;
-                case "sort":
-                    sort();
-                    break;
-                case "find":
-                    find();
-                    break;
-                case "avg":
-                    avg();
-                    break;
-                case "exit":
-                    System.out.println("Exiting program...");
-                    break;
-                default:
-                    System.out.println("Invalid command!");
-            }
-        } while (!command.equals("exit"));
-
+                switch (command_full[0]) {
+                    case "print":
+                        print();
+                        break;
+                    case "add":
+                        add(command_full[1]);
+                        break;
+                    case "del":
+                        del(command_full[1]);
+                        break;
+                    case "edit":
+                        edit(command_full[1]);
+                        break;
+                    case "sort":
+                        sort();
+                        break;
+                    case "find":
+                        find(command_full[1]);
+                        break;
+                    case "avg":
+                        avg();
+                        break;
+                    case "exit":
+                        System.out.println("Exiting program...");
+                        break;
+                    default:
+                        System.out.println("Invalid command!");
+                }
+                }catch (ArrayIndexOutOfBoundsException e){
+                    System.out.println("Wrong command!");
+                }
+            } while (!command.equals("exit"));
+        
         input.close();
     }
 
     private static void print() {
         // code to display all records in the db.csv file
+        String[][][] array3D = read_csv(FILE_NAME, 6);
+        // print the 3D array
+        System.out.println("------------------------------------------------------------");
+        System.out.format("%-4s%-21s%-11s%-6s%-10s%-8s%n", "ID", "City", "Date", "Days", "Price", "Vehicle");
+        System.out.println("------------------------------------------------------------");
+        for (int i = 0; i < array3D.length; i++) {
+            System.out.format("%-4s%-21s%-11s%-6s%-10s%-8s%n", array3D[i][0][0], array3D[i][1][0], array3D[i][2][0],
+                    array3D[i][3][0], array3D[i][4][0], array3D[i][5][0]);
+        }
+        System.out.println("------------------------------------------------------------");
     }
 
     public static void add(String field) {
@@ -240,27 +252,92 @@ public class Main {
         }
     }
 
-    private static void sort() {
-        // code to sort records in the db.csv file by date
+    public static void sort() {
+        String[][][] array3D = read_csv(FILE_NAME, 6);
+        Arrays.sort(array3D, Comparator.comparing(a -> a[2][0]));
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter("db.csv"))) {
+            for (int i = 0; i < array3D.length; i++) {
+                writer.write(array3D[i][0][0] + ";" + array3D[i][1][0] + ";" + array3D[i][2][0] + ";" + array3D[i][3][0] + ";" + array3D[i][4][0] + ";" + array3D[i][5][0]);
+                writer.newLine();
+            }
+        } catch (IOException e) {
+            System.out.println("error writing to file");
+        }
+        System.out.println("sorted");
     }
 
-    private static void find() {
-        // code to find records in the db.csv file with a price lower or equal to the given price
+    public static void find(String tokens) {
+        String[] fields = tokens.split(" ");
+        double targetPrice = Double.parseDouble(fields[0]);
+
+        String[][][] trips = read_csv(FILE_NAME, 6);
+        boolean found = false;
+
+        System.out.println("------------------------------------------------------------");
+        System.out.format("%-4s%-21s%-11s%-6s%-10s%-8s%n", "ID", "City", "Date", "Days", "Price", "Vehicle");
+        System.out.println("------------------------------------------------------------");
+
+        for (int i = 0; i < trips.length; i++) {
+            double price = Double.parseDouble(trips[i][4][0]);
+            if (price <= targetPrice) {
+                System.out.format("%-4s%-21s%-11s%-6s%-10s%-8s%n",
+                        trips[i][0][0], trips[i][1][0], trips[i][2][0], trips[i][3][0], trips[i][4][0], trips[i][5][0]);
+                found = true;
+            }
+        }
+        if (!found) {
+            System.out.println("No trips found.");
+        }
+        System.out.println("------------------------------------------------------------");
     }
 
     private static void avg() {
-        // code to calculate the average price of all records in the db.csv file
+        String[][][] array3D = read_csv(FILE_NAME, 6);
+
+        // calculate average price
+        double sum = 0.0;
+        int count = 0;
+        for (int i = 0; i < array3D.length; i++) {
+            try {
+                double price = Double.parseDouble(array3D[i][4][0]);
+                sum += price;
+                count++;
+            } catch (NumberFormatException e) {
+                // ignore malformed prices
+            }
+        }
+        if (count == 0) {
+            System.out.println("No valid prices found.");
+        } else {
+            double avg = sum / count;
+            System.out.printf("Average price: %.2f\n", avg);
+        }
     }
-//    public static void readDatabase() {
-//        try {
-//            BufferedReader reader = new BufferedReader(new FileReader(FILE_NAME));
-//            while ((line = reader.readLine()) != null) {
-//                //read file
-//            }
-//            reader.close();
-//        } catch (IOException e) {
-//            e.printStackTrace();
-//        }
-//        return trips;
-//    }
+
+    private static String[][][] read_csv(String filename, int collums) {
+        String line;
+        int rowCount = 0;
+        try (BufferedReader br = new BufferedReader(new FileReader(filename))) {
+            while ((line = br.readLine()) != null) {
+                rowCount++;
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        String[][][] array3D = new String[rowCount][collums][];
+        try (BufferedReader br = new BufferedReader(new FileReader(filename))) {
+            int i = 0;
+            while ((line = br.readLine()) != null) {
+                String[] row = line.split(";");
+                for (int j = 0; j < 6; j++) {
+                    array3D[i][j] = new String[]{row[j]};
+                }
+                i++;
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return array3D;
+    }
 }
